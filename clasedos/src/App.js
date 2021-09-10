d0d61630a0a12664f691c8dbb48188c6b95dd2c3
Lylhium <agustinpfarherr@gmail.com>
2021-09-10 02:00:54 -0300

    commit 4

--- clasedos/src/App.js
+++ clasedos/src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Card from './components/Card/card';
+import Footer from './components/footer/Footer';
+import Maps from './components/Maps/maps';
 function App() {
 
 return (
-<div className= "prueba">
-<NavBar></NavBar>
 
-<h1 className="title"> Bienvenido a  CocoLight !</h1>
+<div className= "prueba">
+<NavBar/>
+
+<div className="title"> Bienvenido a CocoLight !</div>
+
+<Card/>
+
+<Maps/>
+
+<Footer/>
+
+
 
 </div>
- 
 
 );
 }
---
+++ clasedos/src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+
+import './footer.css';
+
+//externo materialui
+
+
+const Footer = () => (
+    <div className="footer">
+      <div className="footer-text">Nuestras Redes</div>
+      <ul className="links-footer">
+          <li> <a href="https://web.whatsapp.com/">Whatsapp</a></li>
+          <li> <a href="https://www.instagram.com/">Instagram</a></li>
+          <li> <a href="https://www.facebook.com">Facebook</a></li>
+      </ul>
+    </div>
+  );
+
+
+export 
+default  Footer;
